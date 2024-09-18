a80917f56c3f7bb602b854d8f994f5df967b7631
Hemant Sahu <sahuhemant360@gmail.com>
2024-09-18 19:55:42 +0530

    Edit gemfile and resolve configure of cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,5 @@
 # config/initializers/cors.rb
-
+require 'rack/cors'
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:5173'  # Replace with the actual domain for production
